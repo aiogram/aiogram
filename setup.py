3a2fafae2b1f323446e64c944758c5b5792f7510
Alex Root Junior <jroot.junior@gmail.com>
2017-05-19 19:11:42 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+from setuptools import PackageFinder
+
+from aiogram import __version__ as version
+
+
+def get_description():
+    with open('README.rst', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='aiogram',
+    version=version,
+    packages=PackageFinder.find(),
+    url='https://bitbucket.org/illemius/aiogram',
+    license='MIT',
+    author='Alex Root Junior',
+    author_email='jroot.junior@gmail.com',
+    description='Telegram bot API framework based on asyncio',
+    long_description=get_description(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Framework :: AsyncIO',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=['aiohttp']
+)
