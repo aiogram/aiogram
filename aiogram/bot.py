9bd66a825dff6edf05075d930314834fcd42e322
Alex Root Junior <jroot.junior@gmail.com>
2017-05-21 22:51:48 +0300

    Fuck windows.

--- aiogram/bot.py
+++ aiogram/bot.py
@@ -1,14 +1,13 @@
 import asyncio
-import signal
 
 import aiohttp
 
-from aiogram.utils.payload import generate_payload
 from . import api
 from .api import ApiMethods
 from .types.chat import Chat
 from .types.update import Update
 from .types.user import User
+from .utils.payload import generate_payload
 
 
 class AIOGramBot:
@@ -29,9 +28,7 @@ class AIOGramBot:
             connector=aiohttp.TCPConnector(limit=connections_limit),
             loop=self.loop)
 
-        self.loop.add_signal_handler(signal.SIGINT, self._on_exit)
-
-    def _on_exit(self):
+    def __del__(self):
         self.session.close()
 
     @property
