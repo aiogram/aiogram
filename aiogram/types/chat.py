e244d0214f6b567de3140857db4f2f2165d71ab4
Alex Root Junior <jroot.junior@gmail.com>
2017-05-21 18:54:20 +0300

    Define chat actions

--- aiogram/types/chat.py
+++ aiogram/types/chat.py
@@ -63,3 +63,16 @@ class ChatType:
     GROUP = 'group'
     SUPER_GROUP = 'supergroup'
     CHANNEL = 'channel'
+
+
+class ChatActions:
+    TYPING = 'typing'
+    UPLOAD_PHOTO = 'upload_photo'
+    RECORD_VIDEO = 'record_video'
+    UPLOAD_VIDEO = 'upload_video'
+    RECORD_AUDIO = 'record_audio'
+    UPLOAD_AUDIO = 'upload_audio'
+    UPLOAD_DOCUMENT = 'upload_document'
+    FIND_LOCATION = 'find_location'
+    RECORD_VIDEO_NOTE = 'record_video_note'
+    UPLOAD_VIDEO_NOTE = 'upload_video_note'
