79edeaf960faf8879fb692ad0d30fa8881212470
Alex Root Junior <jroot.junior@gmail.com>
2017-05-26 05:23:17 +0300

    Oops.

--- aiogram/types/chat_member.py
+++ aiogram/types/chat_member.py
@@ -14,3 +14,19 @@ class ChatMember(Deserializable):
         status = raw_data.get('status')
 
         return ChatMember(user, status)
+
+
+class ChatMemberStatus:
+    CREATOR = 'creator'
+    ADMINISTRATOR = 'administrator'
+    MEMBER = 'member'
+    LEFT = 'left'
+    KICKED = 'kicked'
+
+    @classmethod
+    def is_admin(cls, role):
+        return role in [cls.ADMINISTRATOR, cls.CREATOR]
+
+    @classmethod
+    def is_member(cls, role):
+        return role in [cls.MEMBER, cls.ADMINISTRATOR, cls.CREATOR]
