9b6548d0b8004dcfc03cd95b33b99443f2743c71
Alex Root Junior <jroot.junior@gmail.com>
2017-05-26 09:03:16 +0300

    Change payload generator

--- aiogram/utils/payload.py
+++ aiogram/utils/payload.py
@@ -1,5 +1,7 @@
 DEFAULT_FILTER = ['self']
 
 
-def generate_payload(skip_items=DEFAULT_FILTER, **kwargs):
-    return {key: value for key, value in kwargs.items() if key not in skip_items and value}
+def generate_payload(skip_items=None, **kwargs):
+    if skip_items is None:
+        skip_items = []
+    return {key: value for key, value in kwargs.items() if key not in skip_items + DEFAULT_FILTER and value}
