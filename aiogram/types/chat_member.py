a703fc9587750b12b030f7ff349533e181035be2
Alex Root Junior <jroot.junior@gmail.com>
2017-05-26 04:54:38 +0300

    Add ChatMember

---
+++ aiogram/types/chat_member.py
@@ -0,0 +1,34 @@
+from . import Deserializable
+
+
+class ChatMember(Deserializable):
+    __slots__ = ('user', 'status')
+
+    def __init__(self, user, status):
+        self.user = user
+        self.status = status
+
+    @classmethod
+    def de_json(cls, raw_data):
+        raw_data = cls.check_json(raw_data)
+
+        user = raw_data.get('user')
+        status = raw_data.get('status')
+
+        return ChatMember(user, status)
+
+
+class ChatMemberStatus:
+    CREATOR = 'creator'
+    ADMINISTRATOR = 'administrator'
+    MEMBER = 'member'
+    LEFT = 'left'
+    KICKED = 'kicked'
+
+    @classmethod
+    def is_admin(cls, role):
+        return role in [cls.ADMINISTRATOR, cls.CREATOR]
+
+    @classmethod
+    def is_member(cls, role):
+        return role in [cls.MEMBER, cls.ADMINISTRATOR, cls.CREATOR]
