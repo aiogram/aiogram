3b416a1e0e4f153bba4d618ce616d4e6dac8908f
Alex Root Junior <jroot.junior@gmail.com>
2017-05-30 22:42:50 +0300

    Exclude local variables

--- aiogram/utils/payload.py
+++ aiogram/utils/payload.py
@@ -4,4 +4,7 @@ DEFAULT_FILTER = ['self']
 def generate_payload(exclude=None, **kwargs):
     if exclude is None:
         exclude = []
-    return {key: value for key, value in kwargs.items() if key not in exclude + DEFAULT_FILTER and value}
+    return {key: value for key, value in kwargs.items() if
+            key not in exclude + DEFAULT_FILTER
+            and value
+            and not key.startswith('_')}
