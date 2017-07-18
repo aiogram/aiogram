e4c89d0413c1e9bd61e946554a346b8eb922df8a
Alex Root Junior <jroot.junior@gmail.com>
2017-07-19 02:58:17 +0300

    Change version number

--- aiogram/__init__.py
+++ aiogram/__init__.py
@@ -3,8 +3,8 @@ from .bot import Bot
 
 major_version = 0
 minor_version = 3
-build_type = 'b'
-build_number = 1
+build_type = ''
+build_number = ''
 
 
 __version__ = f"{major_version}.{minor_version}{build_type}{build_number}"
