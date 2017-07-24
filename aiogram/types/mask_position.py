a6e90655bc9e06381cfb768e72a9a3417acf8fea
Alex Root Junior <jroot.junior@gmail.com>
2017-07-24 21:45:15 +0300

    Oops. Mask position is Deserializable.

--- aiogram/types/mask_position.py
+++ aiogram/types/mask_position.py
@@ -1,7 +1,7 @@
-from .base import Serializable
+from .base import Deserializable
 
 
-class MaskPosition(Serializable):
+class MaskPosition(Deserializable):
     """
     This object describes the position on faces where a mask should be placed by default.
 
@@ -13,3 +13,14 @@ class MaskPosition(Serializable):
         self.x_shift: float = x_shift
         self.y_shift: float = y_shift
         self.zoom: float = zoom
+
+    @classmethod
+    def de_json(cls, raw_data):
+        raw_data = cls.check_json(raw_data)
+
+        point = raw_data.get('point')
+        x_shift = raw_data.get('x_shift')
+        y_shift = raw_data.get('y_shift')
+        zoom = raw_data.get('zoom')
+
+        return cls(point=point, x_shift=x_shift, y_shift=y_shift, zoom=zoom)
