3eba1c25c742969da2152ca422c7afe2368121f1
Alex Root Junior <jroot.junior@gmail.com>
2017-07-25 04:10:22 +0300

    Change version util and version number.

--- aiogram/utils/versions.py
+++ aiogram/utils/versions.py
@@ -1,4 +1,10 @@
+import datetime
+import os
+import subprocess
+
 from .helper import Helper, HelperMode, Item
+
+# Based on https://github.com/django/django/blob/master/django/utils/version.py
 
 
 class Version:
@@ -40,6 +46,10 @@ class Version:
     def build(self):
         return self.__raw_version[4]
 
+    @property
+    def raw_version(self):
+        return self.raw_version
+
     def get_version(self):
         """
         Returns a PEP 440-compliant version number from VERSION.
@@ -56,9 +66,13 @@ class Version:
         main = self.get_main_version()
 
         sub = ''
-
-        if version[3] != 'final':
-            mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'rc'}
+        print(version[3], Stage.DEV, version[3] == Stage.DEV)
+        if version[3] == Stage.DEV and version[4] == 0:
+            git_changeset = self.get_git_changeset()
+            if git_changeset:
+                sub = '.dev{0}'.format(git_changeset)
+        elif version[3] != Stage.FINAL:
+            mapping = {Stage.ALPHA: 'a', Stage.BETA: 'b', Stage.RC: 'rc', Stage.DEV: 'dev'}
             sub = mapping[version[3]] + str(version[4])
 
         return str(main + sub)
@@ -72,6 +86,26 @@ class Version:
         version = self.__raw_version
         parts = 2 if version[2] == 0 else 3
         return '.'.join(str(x) for x in version[:parts])
+
+    def get_git_changeset(self):
+        """Return a numeric identifier of the latest git changeset.
+        The result is the UTC timestamp of the changeset in YYYYMMDDHHMMSS format.
+        This value isn't guaranteed to be unique, but collisions are very unlikely,
+        so it's sufficient for generating the development version numbers.
+        """
+        repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        print(repo_dir)
+        git_log = subprocess.Popen(
+            'git log --pretty=format:%ct --quiet -1 HEAD',
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            shell=True, cwd=repo_dir, universal_newlines=True,
+        )
+        timestamp = git_log.communicate()[0]
+        try:
+            timestamp = datetime.datetime.utcfromtimestamp(int(timestamp))
+        except ValueError:
+            return None
+        return timestamp.strftime('%Y%m%d%H%M%S')
 
     def __str__(self):
         return self.version
@@ -87,3 +121,4 @@ class Stage(Helper):
     ALPHA = Item()
     BETA = Item()
     RC = Item()
+    DEV = Item()
