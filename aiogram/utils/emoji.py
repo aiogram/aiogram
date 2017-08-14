30874f6dc0628fe5503614644c1d725dc2bba26a
Alex Root Junior <jroot.junior@gmail.com>
2017-08-14 22:53:42 +0300

    Changed his mind.

--- aiogram/utils/emoji.py
+++ aiogram/utils/emoji.py
@@ -1,9 +1,5 @@
-from .deprecated import warn_deprecated
-
 try:
     import emoji
-
-    warn_deprecated('Use emoji module instead that util')
 except ImportError:
     raise ImportError('Need install "emoji" module.')
 
