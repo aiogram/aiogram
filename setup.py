7e53111773a34b10e38bd16b4a8650a30711a983
Alex Root Junior <jroot.junior@gmail.com>
2017-09-12 00:29:04 +0300

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -1,13 +1,42 @@
+#!/usr/bin/env python3
+
+import string
 from distutils.core import setup
 
 from setuptools import PackageFinder
 
 from aiogram import __version__ as version
 
+ALLOWED_SYMBOLS = string.ascii_letters + string.digits + '_-'
+
 
 def get_description():
+    """
+    Read full description from 'README.rst'
+
+    :return: description
+    :rtype: str
+    """
     with open('README.rst', encoding='utf-8') as f:
         return f.read()
+
+
+def get_requirements():
+    """
+    Read requirements from 'requirements txt'
+
+    :return: requirements
+    :rtype: list
+    """
+    requirements = []
+    with open('requirements.txt', 'r') as file:
+        for line in file.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
 
 
 setup(
@@ -18,15 +47,15 @@ setup(
     license='MIT',
     author='Alex Root Junior',
     author_email='jroot.junior@gmail.com',
-    description='Telegram bot API framework based on asyncio',
+    description='Is are pretty simple and fully asynchronously library for Telegram Bot API',
     long_description=get_description(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3.6',
         'Environment :: Console',
         'Framework :: AsyncIO',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'License :: OSI Approved :: MIT License',
     ],
-    install_requires=['aiohttp']
+    install_requires=get_requirements()
 )
