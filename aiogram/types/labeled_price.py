c9b521314426a51b24edbe64d4b54236126da01e
Alex Root Junior <jroot.junior@gmail.com>
2017-10-23 16:23:52 +0300

    Add __init__ in LabeledPrice object.

--- aiogram/types/labeled_price.py
+++ aiogram/types/labeled_price.py
@@ -10,3 +10,6 @@ class LabeledPrice(base.TelegramObject):
     """
     label: base.String = fields.Field()
     amount: base.Integer = fields.Field()
+
+    def __init__(self, label: base.String, amount: base.Integer):
+        super(LabeledPrice, self).__init__(label=label, amount=amount)
