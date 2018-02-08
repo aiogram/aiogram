7c9290784016c087b0bc36f6617843b9a5995155
Alex Root Junior <jroot.junior@gmail.com>
2018-02-08 21:19:06 +0200

    Upd description of `exportChatInviteLink`

--- aiogram/bot/bot.py
+++ aiogram/bot/bot.py
@@ -907,8 +907,8 @@ class Bot(BaseBot):
 
     async def export_chat_invite_link(self, chat_id: typing.Union[base.Integer, base.String]) -> base.String:
         """
-        Use this method to export an invite link to a supergroup or a channel. 
-        The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. 
+        Use this method to generate a new invite link for a chat; any previously generated link is revoked.
+        The bot must be an administrator in the chat for this to work and must have the appropriate admin rights.
 
         Source: https://core.telegram.org/bots/api#exportchatinvitelink
 
