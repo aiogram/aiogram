b37763c23f7fbf64e6e588a963cf0628eaef7a6f
Alex Root Junior <jroot.junior@gmail.com>
2018-07-14 16:28:07 +0300

    Allowed to more simply import base utils from aiogram

--- aiogram/__init__.py
+++ aiogram/__init__.py
@@ -1,8 +1,16 @@
 import asyncio
 import os
 
+from . import bot
+from . import contrib
+from . import dispatcher
+from . import types
+from . import utils
 from .bot import Bot
 from .dispatcher import Dispatcher
+from .dispatcher import filters
+from .dispatcher import middlewares
+from .utils import exceptions, executor, helper, markdown as md
 
 try:
     import uvloop
@@ -11,6 +19,24 @@ except ImportError:
 else:
     if 'DISABLE_UVLOOP' not in os.environ:
         asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+__all__ = [
+    'Bot',
+    'Dispatcher',
+    '__api_version__',
+    '__version__',
+    'bot',
+    'contrib',
+    'dispatcher',
+    'exceptions',
+    'executor',
+    'filters',
+    'helper',
+    'md',
+    'middlewares',
+    'types',
+    'utils'
+]
 
 __version__ = '2.0.dev1'
 __api_version__ = '3.6'
--- aiogram/dispatcher/__init__.py
+++ aiogram/dispatcher/__init__.py
@@ -3,9 +3,10 @@ from . import handler
 from . import middlewares
 from . import storage
 from . import webhook
-from .dispatcher import Dispatcher, dispatcher, FSMContext
+from .dispatcher import Dispatcher, dispatcher, FSMContext, DEFAULT_RATE_LIMIT
 
 __all__ = [
+    'DEFAULT_RATE_LIMIT',
     'Dispatcher',
     'dispatcher',
     'FSMContext',
