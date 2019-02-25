7c0ada58977d20729e11ed6d8bfa8ee165d4dd84
Oleg A <t0rr@mail.ru>
2019-02-25 15:43:18 +0300

    Replaced 'ssl_context' kwarg with 'ssl' cause: DeprecationWarning: ssl_context is deprecated, use ssl=context instead

--- aiogram/bot/base.py
+++ aiogram/bot/base.py
@@ -77,8 +77,7 @@ class BaseBot:
             self.proxy = None
             self.proxy_auth = None
         else:
-            connector = aiohttp.TCPConnector(limit=connections_limit, ssl_context=ssl_context,
-                                             loop=self.loop)
+            connector = aiohttp.TCPConnector(limit=connections_limit, ssl=ssl_context, loop=self.loop)
 
         self.session = aiohttp.ClientSession(connector=connector, loop=self.loop, json_serialize=json.dumps)
 
