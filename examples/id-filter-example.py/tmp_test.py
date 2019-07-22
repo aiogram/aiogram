c1fc41bd7e4da3cbd2b070197001d70c4b761245
birdi <egor.birdi7@gmail.com>
2019-07-22 04:16:30 +0300

    Add example of usage of IdFilter

---
+++ examples/id-filter-example.py/tmp_test.py
@@ -0,0 +1,46 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.handler import SkipHandler
+
+API_TOKEN = 'API_TOKE_HERE'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+user_id_to_test = None  # todo: Set id here
+chat_id_to_test = user_id_to_test
+
+
+@dp.message_handler(user=user_id_to_test)
+async def handler1(msg: types.Message):
+    await bot.send_message(msg.chat.id,
+                           "Hello, checking with user=")
+    raise SkipHandler
+
+
+@dp.message_handler(user_id=user_id_to_test)
+async def handler2(msg: types.Message):
+    await bot.send_message(msg.chat.id,
+                           "Hello, checking with user_id=")
+    raise SkipHandler
+
+
+@dp.message_handler(chat=chat_id_to_test)
+async def handler3(msg: types.Message):
+    await bot.send_message(msg.chat.id,
+                           "Hello, checking with chat=")
+    raise SkipHandler
+
+
+@dp.message_handler(chat_id=chat_id_to_test)
+async def handler4(msg: types.Message):
+    await bot.send_message(msg.chat.id,
+                           "Hello, checking with chat_id=")
+    raise SkipHandler
+
+
+@dp.message_handler(user=user_id_to_test, chat_id=chat_id_to_test)
+async def handler5(msg: types.Message):
+    await bot.send_message(msg.chat.id,
+                           "Hello from user= & chat_id=")
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
