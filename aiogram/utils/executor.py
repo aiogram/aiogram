f4339d10b043a691aad662b80e9e7752af76213a
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-08-11 22:55:44 +0300

    Super minor refactor webhook and executor

--- aiogram/utils/executor.py
+++ aiogram/utils/executor.py
@@ -15,7 +15,7 @@ from ..dispatcher.webhook import BOT_DISPATCHER_KEY, DEFAULT_ROUTE_NAME, Webhook
 APP_EXECUTOR_KEY = 'APP_EXECUTOR'
 
 
-def _setup_callbacks(executor, on_startup=None, on_shutdown=None):
+def _setup_callbacks(executor: 'Executor', on_startup=None, on_shutdown=None):
     if on_startup is not None:
         executor.on_startup(on_startup)
     if on_shutdown is not None:
