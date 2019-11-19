71def6449f0961d30818bca5964a4c9bfff3f173
Alex Root Junior <jroot.junior@gmail.com>
2019-11-20 01:32:15 +0200

    Make speedups extras [fast] will install uvloop

--- aiogram/__init__.py
+++ aiogram/__init__.py
@@ -2,7 +2,14 @@ from .api import methods, types
 from .api.client import session
 from .api.client.bot import Bot
 
+try:
+    import uvloop
+
+    uvloop.install()
+except ImportError:
+    pass
+
 __all__ = ["__api_version__", "__version__", "types", "methods", "Bot", "session"]
 
-__version__ = "3.0dev.1"
+__version__ = "3.0.0a0"
 __api_version__ = "4.4"
