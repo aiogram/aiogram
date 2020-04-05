12faef50f81df80e7d26ef95151377fb8bc9f5df
Oleg A <t0rr@mail.ru>
2020-04-05 17:55:00 +0300

    #289 updated createNewStickerSet docs

--- aiogram/bot/bot.py
+++ aiogram/bot/bot.py
@@ -1847,18 +1847,26 @@ class Bot(BaseBot, DataMixin, ContextInstanceMixin):
                                      contains_masks: typing.Union[base.Boolean, None] = None,
                                      mask_position: typing.Union[types.MaskPosition, None] = None) -> base.Boolean:
         """
-        Use this method to create new sticker set owned by a user. The bot will be able to edit the created sticker set.
+        Use this method to create a new sticker set owned by a user.
+        The bot will be able to edit the sticker set thus created.
+        You must use exactly one of the fields png_sticker or tgs_sticker.
 
         Source: https://core.telegram.org/bots/api#createnewstickerset
 
         :param user_id: User identifier of created sticker set owner
         :type user_id: :obj:`base.Integer`
-        :param name: Short name of sticker set, to be used in t.me/addstickers/ URLs (e.g., animals)
+        :param name: Short name of sticker set, to be used in t.me/addstickers/ URLs (e.g., animals).
+            Can contain only english letters, digits and underscores.
+            Must begin with a letter, can't contain consecutive underscores and must end in “_by_<bot username>”.
+            <bot_username> is case insensitive. 1-64 characters.
         :type name: :obj:`base.String`
         :param title: Sticker set title, 1-64 characters
         :type title: :obj:`base.String`
-        :param png_sticker: Png image with the sticker, must be up to 512 kilobytes in size,
+        :param png_sticker: PNG image with the sticker, must be up to 512 kilobytes in size,
             dimensions must not exceed 512px, and either width or height must be exactly 512px.
+            Pass a file_id as a String to send a file that already exists on the Telegram servers,
+            pass an HTTP URL as a String for Telegram to get a file from the Internet, or
+            upload a new one using multipart/form-data. More info on https://core.telegram.org/bots/api#sending-files
         :type png_sticker: :obj:`typing.Union[base.InputFile, base.String]`
         :param tgs_sticker: TGS animation with the sticker, uploaded using multipart/form-data.
             See https://core.telegram.org/animated_stickers#technical-requirements for technical requirements
