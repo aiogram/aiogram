79f62f9e613e91f53c646ac2790bc41d58552f3c
evgfilim1 <evgfilim1@yandex.ru>
2020-04-06 18:20:39 +0500

    Check whether Python is 3.7+

--- aiogram/__init__.py
+++ aiogram/__init__.py
@@ -1,3 +1,8 @@
+import sys
+if sys.version_info < (3, 7):
+    raise ImportError('Your Python version {0} is not supported by aiogram, please install '
+                      'Python 3.7+'.format('.'.join(map(str, sys.version_info[:3]))))
+
 import asyncio
 import os
 
