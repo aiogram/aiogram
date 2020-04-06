97bda44718397e050e68f9f64516d4d79ce032d0
Oleg A <t0rr@mail.ru>
2020-04-06 23:59:37 +0300

    #289 BotCommand type init fix

--- aiogram/types/bot_command.py
+++ aiogram/types/bot_command.py
@@ -10,3 +10,6 @@ class BotCommand(base.TelegramObject):
     """
     command: base.String = fields.Field()
     description: base.String = fields.Field()
+
+    def __init__(self, command: base.String, description: base.String):
+        super(BotCommand, self).__init__(command=command, description=description)
