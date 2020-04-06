27e02e007c9f658a069cce2b65c214e6281503cf
Alex Root Junior <jroot.junior@gmail.com>
2020-04-07 00:59:59 +0300

    Bump version

--- aiogram/bot/api.py
+++ aiogram/bot/api.py
@@ -153,7 +153,7 @@ class Methods(Helper):
     """
     Helper for Telegram API Methods listed on https://core.telegram.org/bots/api
 
-    List is updated to Bot API 4.6
+    List is updated to Bot API 4.7
     """
     mode = HelperMode.lowerCamelCase
 
