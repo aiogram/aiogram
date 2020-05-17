66ecc6996dd7bf5fa7561ee1ff9d2985bf4422e3
Gabben <43146729+gabbhack@users.noreply.github.com>
2020-05-17 17:01:06 +0500

    Improving test_callback_query

--- tests/test_api/test_types/test_callback_query.py
+++ tests/test_api/test_types/test_callback_query.py
@@ -8,7 +8,12 @@ class TestCallbackQuery:
             id="id", from_user=User(id=42, is_bot=False, first_name="name"), chat_instance="chat"
         )
 
-        api_method = callback_query.answer()
+        kwargs = dict(text="foo", show_alert=True, url="https://foo.bar/", cache_time=123)
+
+        api_method = callback_query.answer(**kwargs)
 
         assert isinstance(api_method, AnswerCallbackQuery)
         assert api_method.callback_query_id == callback_query.id
+
+        for key, value in kwargs.items():
+            assert getattr(api_method, key) == value
