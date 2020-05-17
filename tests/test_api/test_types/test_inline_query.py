a6f83599c8c5ce9adf0ab550b44a29fe06be68d6
Gabben <43146729+gabbhack@users.noreply.github.com>
2020-05-17 17:03:35 +0500

    Improving test_inline_query

--- tests/test_api/test_types/test_inline_query.py
+++ tests/test_api/test_types/test_inline_query.py
@@ -11,7 +11,12 @@ class TestInlineQuery:
             offset="",
         )
 
-        api_method = inline_query.answer([])
+        kwargs = dict(results=[], cache_time=123, next_offset="123", switch_pm_text="foo", switch_pm_parameter="foo")
+
+        api_method = inline_query.answer(**kwargs)
 
         assert isinstance(api_method, AnswerInlineQuery)
         assert api_method.inline_query_id == inline_query.id
+
+        for key, value in kwargs.items():
+            assert getattr(api_method, key) == value
