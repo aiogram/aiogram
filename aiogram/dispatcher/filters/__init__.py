aab1219c4ce86f7887aae555cc900d676cf67f90
Egor <egor.birdi7@gmail.com>
2020-06-14 17:17:08 +0500

    fix: add import in filters/__init__

--- aiogram/dispatcher/filters/__init__.py
+++ aiogram/dispatcher/filters/__init__.py
@@ -1,7 +1,7 @@
 from .builtin import Command, CommandHelp, CommandPrivacy, CommandSettings, CommandStart, ContentTypesFilter, \
     ExceptionsFilter, HashTag, Regexp, RegexpCommandsFilter, StateFilter, \
     Text, IDFilter, AdminFilter, IsReplyFilter, IsSenderContact, ForwardedMessageFilter, \
-    ContentTypesFilter
+    ContentTypesFilter, ChatTypesFilter
 from .factory import FiltersFactory
 from .filters import AbstractFilter, BoundFilter, Filter, FilterNotPassed, FilterRecord, execute_filter, \
     check_filters, get_filter_spec, get_filters_spec
@@ -35,5 +35,5 @@ __all__ = [
     'check_filters',
     'ForwardedMessageFilter',
     'ContentTypesFilter',
-    
+    'ChatTypesFilter',
 ]
