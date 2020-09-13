075d8be72dc301f8e6729ea89257f6fd0cda7c41
Ramzan Bekbulatov <bekbulatov.ramzan@ya.ru>
2020-09-13 22:13:15 +0300

    Add missed emoji argument to reply_dice (#395)
    * fix: add missed emoji argument to reply_dice
    
    * ref: order arguments for send_dice

--- aiogram/types/message.py
+++ aiogram/types/message.py
@@ -1048,8 +1048,8 @@ class Message(base.TelegramObject):
         """
         return await self.bot.send_dice(
             chat_id=self.chat.id,
-            disable_notification=disable_notification,
             emoji=emoji,
+            disable_notification=disable_notification,
             reply_to_message_id=self.message_id if reply else None,
             reply_markup=reply_markup,
         )
@@ -1834,6 +1834,7 @@ class Message(base.TelegramObject):
         """
         return await self.bot.send_dice(
             chat_id=self.chat.id,
+            emoji=emoji,
             disable_notification=disable_notification,
             reply_to_message_id=self.message_id if reply else None,
             reply_markup=reply_markup,
