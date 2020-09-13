7d1c8c42d3cdd333e3a2933eb09fcd9523810852
Alex Root Junior <jroot.junior@gmail.com>
2020-09-13 22:27:50 +0300

    Fix syntax error

--- aiogram/types/message.py
+++ aiogram/types/message.py
@@ -245,8 +245,8 @@ class Message(base.TelegramObject):
 
         :return: str
         """
-        if self.chat.type == ChatType.PRIVATE:
-            raise TypeError('Invalid chat type
+        if ChatType.is_private(self.chat):
+            raise TypeError("Invalid chat type!")
         url = "https://t.me/"
         if self.chat.username:
             # Generates public link
