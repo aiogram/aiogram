2ce11bde0d810f17fc487c22b8a429ef458edb0e
uburuntu <bekbulatov.ramzan@ya.ru>
2020-10-05 01:58:07 +0300

    new: football dice emoji

--- aiogram/types/dice.py
+++ aiogram/types/dice.py
@@ -6,7 +6,7 @@ class Dice(base.TelegramObject):
     This object represents a dice with random value from 1 to 6.
     (Yes, we're aware of the “proper” singular of die.
     But it's awkward, and we decided to help it change. One dice at a time!)
-    
+
     https://core.telegram.org/bots/api#dice
     """
     emoji: base.String = fields.Field()
@@ -17,3 +17,4 @@ class DiceEmoji:
     DICE = '🎲'
     DART = '🎯'
     BASKETBALL = '🏀'
+    FOOTBALL = '⚽'
