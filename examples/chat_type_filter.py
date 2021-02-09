4701b852bde1f0541c303e93fea8a3f6c1524a0b
Yan Khachko <luck207869@gmail.com>
2021-02-09 11:38:51 +0000

    Update chat_type_filter.py

--- examples/chat_type_filter.py
+++ examples/chat_type_filter.py
@@ -22,8 +22,8 @@ dp = Dispatcher(bot)
 @dp.message_handler(chat_type=[ChatType.PRIVATE, ChatType.CHANNEL])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
-    """
+ This handler will be called when user sends message in private chat or channel
+ """
     await message.reply("Hi!\nI'm hearing your messages in private chats and channels")
 
     # propagate message to the next handler
@@ -33,7 +33,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(chat_type=ChatType.PRIVATE)
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
+    This handler will be called when user sends message in private chat
     """
     await message.reply("Hi!\nI'm hearing your messages only in private chats")
 
