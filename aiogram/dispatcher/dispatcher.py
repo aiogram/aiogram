d67011605cf00d4afdd497b97f80f4d18e5c0cb6
Oleg A <t0rr@mail.ru>
2021-03-14 19:33:44 +0300

    feat: pass handler task to result

--- aiogram/dispatcher/dispatcher.py
+++ aiogram/dispatcher/dispatcher.py
@@ -1199,6 +1199,7 @@ class Dispatcher(DataMixin, ContextInstanceMixin):
         async def wrapper(*args, **kwargs):
             task = self._loop_create_task(func(*args, **kwargs))
             task.add_done_callback(process_response)
+            return task
 
         return wrapper
 
