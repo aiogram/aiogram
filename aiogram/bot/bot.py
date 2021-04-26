af43769da72e8cb8b4363a179cfbaadd9331b05c
Oleg A <t0rr@mail.ru>
2021-04-26 18:33:48 +0300

    fix: sendChatAction documentation update

--- aiogram/bot/bot.py
+++ aiogram/bot/bot.py
@@ -1484,19 +1484,36 @@ class Bot(BaseBot, DataMixin, ContextInstanceMixin):
     async def send_chat_action(self, chat_id: typing.Union[base.Integer, base.String],
                                action: base.String) -> base.Boolean:
         """
-        Use this method when you need to tell the user that something is happening on the bot's side.
-        The status is set for 5 seconds or less
-        (when a message arrives from your bot, Telegram clients clear its typing status).
+        Use this method when you need to tell the user that something is
+        happening on the bot's side. The status is set for 5 seconds or
+        less (when a message arrives from your bot, Telegram clients
+        clear its typing status). Returns True on success.
 
-        We only recommend using this method when a response from the bot will take
-        a noticeable amount of time to arrive.
+        Example: The ImageBot needs some time to process a request and
+        upload the image. Instead of sending a text message along the
+        lines of “Retrieving image, please wait…”, the bot may use
+        sendChatAction with action = upload_photo. The user will see a
+        “sending photo” status for the bot.
+
+        We only recommend using this method when a response from the bot
+        will take a noticeable amount of time to arrive.
 
         Source: https://core.telegram.org/bots/api#sendchataction
 
-        :param chat_id: Unique identifier for the target chat or username of the target channel
+        :param chat_id: Unique identifier for the target chat or
+            username of the target channel (in the format
+            @channelusername)
         :type chat_id: :obj:`typing.Union[base.Integer, base.String]`
-        :param action: Type of action to broadcast
+
+        :param action: Type of action to broadcast. Choose one,
+            depending on what the user is about to receive: `typing` for
+            text messages, `upload_photo` for photos, `record_video` or
+            `upload_video` for videos, `record_voice` or `upload_voice`
+            for voice notes, `upload_document` for general files,
+            `find_location` for location data, `record_video_note` or
+            `upload_video_note` for video notes.
         :type action: :obj:`base.String`
+
         :return: Returns True on success
         :rtype: :obj:`base.Boolean`
         """
