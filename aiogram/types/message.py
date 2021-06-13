b9f3299cc9d99b1972161c13bc9e4c946d95f9d3
Biorobot1337 <83316072+genagorkin1@users.noreply.github.com>
2021-06-13 22:37:53 +0500

    Update message.py
    Fixed syntax bug, added comma

--- aiogram/types/message.py
+++ aiogram/types/message.py
@@ -2933,7 +2933,7 @@ class Message(base.TelegramObject):
                 question=self.poll.question,
                 options=[option.text for option in self.poll.options],
                 is_anonymous=self.poll.is_anonymous,
-                allows_multiple_answers=self.poll.allows_multiple_answers
+                allows_multiple_answers=self.poll.allows_multiple_answers,
                 **kwargs,
             )
         elif self.dice:
