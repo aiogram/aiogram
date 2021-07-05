5e265727d331f14096d74ed77a1abe2db62c3aa6
Alex Root Junior <jroot.junior@gmail.com>
2021-07-05 03:06:36 +0300

    Update code

--- aiogram/methods/get_chat_administrators.py
+++ aiogram/methods/get_chat_administrators.py
@@ -2,21 +2,50 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Dict, List, Union
 
-from ..types import ChatMember
+from ..types import (
+    ChatMemberAdministrator,
+    ChatMemberBanned,
+    ChatMemberLeft,
+    ChatMemberMember,
+    ChatMemberOwner,
+    ChatMemberRestricted,
+)
 from .base import Request, TelegramMethod
 
 if TYPE_CHECKING:  # pragma: no cover
     from ..client.bot import Bot
 
 
-class GetChatAdministrators(TelegramMethod[List[ChatMember]]):
+class GetChatAdministrators(
+    TelegramMethod[
+        List[
+            Union[
+                ChatMemberOwner,
+                ChatMemberAdministrator,
+                ChatMemberMember,
+                ChatMemberRestricted,
+                ChatMemberLeft,
+                ChatMemberBanned,
+            ]
+        ]
+    ]
+):
     """
     Use this method to get a list of administrators in a chat. On success, returns an Array of :class:`aiogram.types.chat_member.ChatMember` objects that contains information about all chat administrators except other bots. If the chat is a group or a supergroup and no administrators were appointed, only the creator will be returned.
 
     Source: https://core.telegram.org/bots/api#getchatadministrators
     """
 
-    __returning__ = List[ChatMember]
+    __returning__ = List[
+        Union[
+            ChatMemberOwner,
+            ChatMemberAdministrator,
+            ChatMemberMember,
+            ChatMemberRestricted,
+            ChatMemberLeft,
+            ChatMemberBanned,
+        ]
+    ]
 
     chat_id: Union[int, str]
     """Unique identifier for the target chat or username of the target supergroup or channel (in the format :code:`@channelusername`)"""
