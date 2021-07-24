16eac753321241f56b46fd08aba42a0afb7191ca
evgfilim1 <evgfilim1@yandex.ru>
2021-07-24 17:04:55 +0500

    Test new and renamed API methods

---
+++ tests/test_api/test_methods/test_ban_chat_member.py
@@ -0,0 +1,24 @@
+import pytest
+
+from aiogram.methods import BanChatMember, Request
+from tests.mocked_bot import MockedBot
+
+
+class TestKickChatMember:
+    @pytest.mark.asyncio
+    async def test_method(self, bot: MockedBot):
+        prepare_result = bot.add_result_for(BanChatMember, ok=True, result=True)
+
+        response: bool = await BanChatMember(chat_id=-42, user_id=42)
+        request: Request = bot.get_request()
+        assert request.method == "banChatMember"
+        assert response == prepare_result.result
+
+    @pytest.mark.asyncio
+    async def test_bot_method(self, bot: MockedBot):
+        prepare_result = bot.add_result_for(BanChatMember, ok=True, result=True)
+
+        response: bool = await bot.ban_chat_member(chat_id=-42, user_id=42)
+        request: Request = bot.get_request()
+        assert request.method == "banChatMember"
+        assert response == prepare_result.result
---
+++ tests/test_api/test_methods/test_get_chat_member_count.py
@@ -0,0 +1,24 @@
+import pytest
+
+from aiogram.methods import GetChatMemberCount, Request
+from tests.mocked_bot import MockedBot
+
+
+class TestGetChatMembersCount:
+    @pytest.mark.asyncio
+    async def test_method(self, bot: MockedBot):
+        prepare_result = bot.add_result_for(GetChatMemberCount, ok=True, result=42)
+
+        response: int = await GetChatMemberCount(chat_id=-42)
+        request: Request = bot.get_request()
+        assert request.method == "getChatMemberCount"
+        assert response == prepare_result.result
+
+    @pytest.mark.asyncio
+    async def test_bot_method(self, bot: MockedBot):
+        prepare_result = bot.add_result_for(GetChatMemberCount, ok=True, result=42)
+
+        response: int = await bot.get_chat_member_count(chat_id=-42)
+        request: Request = bot.get_request()
+        assert request.method == "getChatMemberCount"
+        assert response == prepare_result.result
