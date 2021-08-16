71eb5fc44ebde015cef6618d21daac7ce0d13204
darksidecat <58224121+darksidecat@users.noreply.github.com>
2021-08-17 00:08:36 +0300

    fix #660 prepare parse mode for input_message_content (#661)
    * fix #660 prepare parse mode for input_message_content of InlineQueryResult
    
    * remove redundant get
    
    * black lint
    
    * add towncrier patch note

--- tests/test_api/test_methods/test_answer_inline_query.py
+++ tests/test_api/test_methods/test_answer_inline_query.py
@@ -2,7 +2,7 @@ import pytest
 
 from aiogram import Bot
 from aiogram.methods import AnswerInlineQuery, Request
-from aiogram.types import InlineQueryResult, InlineQueryResultPhoto
+from aiogram.types import InlineQueryResult, InlineQueryResultPhoto, InputTextMessageContent
 from tests.mocked_bot import MockedBot
 
 pytestmark = pytest.mark.asyncio
@@ -40,3 +40,22 @@ class TestAnswerInlineQuery:
         new_bot = Bot(token="42:TEST", parse_mode="HTML")
         request = query.build_request(new_bot)
         assert request.data["results"][0]["parse_mode"] == "HTML"
+
+    def test_parse_mode_input_message_content(self, bot: MockedBot):
+        query = AnswerInlineQuery(
+            inline_query_id="query id",
+            results=[
+                InlineQueryResultPhoto(
+                    id="result id",
+                    photo_url="photo",
+                    thumb_url="thumb",
+                    input_message_content=InputTextMessageContent(message_text="test"),
+                )
+            ],
+        )
+        request = query.build_request(bot)
+        assert request.data["results"][0]["input_message_content"]["parse_mode"] is None
+
+        new_bot = Bot(token="42:TEST", parse_mode="HTML")
+        request = query.build_request(new_bot)
+        assert request.data["results"][0]["input_message_content"]["parse_mode"] == "HTML"
