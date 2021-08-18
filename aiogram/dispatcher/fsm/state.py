61f6e05be46b0b48c2a280e565a06b326ce372f4
Andrey Tikhonov <andrey.tikhonov@quantori.com>
2021-08-18 10:48:43 +0300

    fix type hint

--- aiogram/dispatcher/fsm/state.py
+++ aiogram/dispatcher/fsm/state.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Any, Optional, Tuple, Type, no_type_check
+from typing import Any, Iterator, Optional, Tuple, Type, no_type_check
 
 from ...types import TelegramObject
 
@@ -118,7 +118,7 @@ class StatesGroupMeta(type):
     def __str__(self) -> str:
         return f"<StatesGroup '{self.__full_group_name__}'>"
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[State]:
         return iter(self.__all_states__)
 
 
