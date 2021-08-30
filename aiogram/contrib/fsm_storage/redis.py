9f78981403a23dfdcbc77af841f23f1153ac67d8
Oleg A <t0rr@mail.ru>
2021-08-30 11:42:52 +0300

    fix: decode aioredis v2 responses

--- aiogram/contrib/fsm_storage/redis.py
+++ aiogram/contrib/fsm_storage/redis.py
@@ -331,6 +331,7 @@ class AioRedisAdapterV2(AioRedisAdapterBase):
                     password=self._password,
                     ssl=self._ssl,
                     max_connections=self._pool_size,
+                    decode_responses=True,
                     **self._kwargs,
                 )
         return self._redis
