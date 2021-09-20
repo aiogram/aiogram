301a43e26b02297fec20b768ac2ce752e690a4d4
samuelfirst <a.a.sheykin@gmail.com>
2021-09-20 23:11:38 +0300

    Set default disable_web_page_preview

--- aiogram/bot/bot.py
+++ aiogram/bot/bot.py
@@ -323,6 +323,8 @@ class Bot(BaseBot, DataMixin, ContextInstanceMixin):
         payload = generate_payload(**locals())
         if self.parse_mode and entities is None:
             payload.setdefault('parse_mode', self.parse_mode)
+        if self.disable_web_page_preview:
+            payload.setdefault('disable_web_page_preview', self.disable_web_page_preview)
 
         result = await self.request(api.Methods.SEND_MESSAGE, payload)
         return types.Message(**result)
@@ -422,7 +424,6 @@ class Bot(BaseBot, DataMixin, ContextInstanceMixin):
         reply_markup = prepare_arg(reply_markup)
         caption_entities = prepare_arg(caption_entities)
         payload = generate_payload(**locals())
-
         if self.parse_mode and caption_entities is None:
             payload.setdefault('parse_mode', self.parse_mode)
 
@@ -2543,6 +2544,8 @@ class Bot(BaseBot, DataMixin, ContextInstanceMixin):
         payload = generate_payload(**locals())
         if self.parse_mode and entities is None:
             payload.setdefault('parse_mode', self.parse_mode)
+        if self.disable_web_page_preview:
+            payload.setdefault('disable_web_page_preview', self.disable_web_page_preview)
 
         result = await self.request(api.Methods.EDIT_MESSAGE_TEXT, payload)
         if isinstance(result, bool):
