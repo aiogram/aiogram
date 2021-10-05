ce8bedb9781898451d639649377b57948f1ae691
darksidecat <58224121+darksidecat@users.noreply.github.com>
2021-10-05 23:40:53 +0300

    update tests

--- tests/test_api/test_client/test_session/test_middlewares/test_request_logging.py
+++ tests/test_api/test_client/test_session/test_middlewares/test_request_logging.py
@@ -18,7 +18,7 @@ class TestRequestLogging:
 
         bot.add_result_for(GetMe, ok=True, result=User(id=42, is_bot=True, first_name="Test"))
         assert await bot.get_me()
-        assert "Make request with method=GetMe by bot id=42" in caplog.text
+        assert "Make request with method='GetMe' by bot id=42" in caplog.text
 
     async def test_ignore_methods(self, bot: MockedBot, caplog):
         caplog.set_level(logging.INFO)
@@ -26,7 +26,7 @@ class TestRequestLogging:
 
         bot.add_result_for(GetMe, ok=True, result=User(id=42, is_bot=True, first_name="Test"))
         assert await bot.get_me()
-        assert "Make request with method=GetMe by bot id=42" not in caplog.text
+        assert "Make request with method='GetMe' by bot id=42" not in caplog.text
 
         bot.add_result_for(
             SendMessage,
@@ -39,4 +39,4 @@ class TestRequestLogging:
             ),
         )
         assert await bot.send_message(chat_id=1, text="Test")
-        assert "Make request with method=SendMessage by bot id=42" in caplog.text
+        assert "Make request with method='SendMessage' by bot id=42" in caplog.text
