14a65b8f06248885e55249e58e90df388be9e943
Ramzan Bekbulatov <bekbulatov.ramzan@ya.ru>
2021-11-06 22:16:50 +0000

    enh: return check but less strict

--- aiogram/bot/bot.py
+++ aiogram/bot/bot.py
@@ -1004,6 +1004,10 @@ class Bot(BaseBot, DataMixin, ContextInstanceMixin):
         # Convert list to MediaGroup
         if isinstance(media, list):
             media = types.MediaGroup(media)
+        
+        # Check MediaGroup quantity
+        if not (1 <= len(media.media) <= 10):
+            raise ValidationError("Media group must include 2-10 items as written in docs, but also it works with 1 element")
 
         files = dict(media.get_files())
 
