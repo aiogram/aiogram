bc0d50c677985d2854ddf6039dcb34a8b0c34ad9
Cyril Margorin <cyril@imperx.com>
2021-11-22 17:02:50 +0300

    Create test case for invalid 'FSMContextProxy.update()' function

---
+++ tests/test_dispatcher/test_fsm_context.py
@@ -0,0 +1,11 @@
+import pytest
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+
+
+class TestFSMContext:
+    @pytest.mark.asyncio
+    async def test_update_data(self):
+        context = FSMContext(MemoryStorage(), chat=1, user=1)
+        async with context.proxy() as data:
+            data.update(key1="value1", key2="value2")
