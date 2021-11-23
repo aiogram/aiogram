2d9cba8f4964ae7867cbc81c03d800baa2acacb5
Cyril Margorin <cyril@imperx.com>
2021-11-23 19:32:36 +0300

    Verify that written data has been stored and read back correctly

--- tests/test_dispatcher/test_fsm_context.py
+++ tests/test_dispatcher/test_fsm_context.py
@@ -9,3 +9,6 @@ class TestFSMContext:
         context = FSMContext(MemoryStorage(), chat=1, user=1)
         async with context.proxy() as data:
             data.update(key1="value1", key2="value2")
+        async with context.proxy() as data:
+            assert data['key1'] == "value1"
+            assert data['key2'] == "value2"
