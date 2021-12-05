5f6dff1d7edce6d3478195dc81d25f606ab859dc
Aryn <63111101+arynyklas@users.noreply.github.com>
2021-12-05 16:43:47 +0600

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -17,30 +17,6 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-
-
-@dp.message_handler(regexp='(^cat[s]?$|puss)')
-async def cats(message: types.Message):
-    with open('data/cats.jpg', 'rb') as photo:
-        '''
-        # Old fashioned way:
-        await bot.send_photo(
-            message.chat.id,
-            photo,
-            caption='Cats are here 😺',
-            reply_to_message_id=message.message_id,
-        )
-        '''
-
-        await message.reply_photo(photo, caption='Cats are here 😺')
-
-
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
