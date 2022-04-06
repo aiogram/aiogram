ffe9506dc52afe3310a2d1138a142d71da304a21
Oleg A <t0rr@mail.ru>
2022-04-06 04:19:31 +0300

    `parse_mode` should be `UNSET` instead of `None` (#877)
    * fix: parse_mode should be UNSET instead of None
    
    #876
    
    * docs: add changelog
    
    * docs: fixed changelog name
    
    * fix: changelog typo fix

--- aiogram/types/message.py
+++ aiogram/types/message.py
@@ -1753,7 +1753,7 @@ class Message(TelegramObject):
         self,
         chat_id: Union[int, str],
         caption: Optional[str] = None,
-        parse_mode: Optional[str] = None,
+        parse_mode: Optional[str] = UNSET,
         caption_entities: Optional[List[MessageEntity]] = None,
         disable_notification: Optional[bool] = None,
         reply_to_message_id: Optional[int] = None,
