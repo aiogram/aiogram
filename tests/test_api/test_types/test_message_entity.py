4a99661b383ced7abd89695da9d113e4e3a87657
Alex Root Junior <jroot.junior@gmail.com>
2022-06-26 00:27:02 +0300

    Rename tests

--- tests/test_api/test_types/test_message_entity.py
+++ tests/test_api/test_types/test_message_entity.py
@@ -3,11 +3,11 @@ from tests.deprecated import check_deprecated
 
 
 class TestMessageEntity:
-    def test_extract(self):
+    def test_extract_from(self):
         entity = MessageEntity(type="hashtag", length=4, offset=5)
         assert entity.extract_from("#foo #bar #baz") == "#bar"
 
-    def test_get_text(self):
+    def test_extract(self):
         entity = MessageEntity(type="hashtag", length=4, offset=5)
         with check_deprecated("3.0b5", exception=AttributeError):
             assert entity.extract("#foo #bar #baz") == "#bar"
