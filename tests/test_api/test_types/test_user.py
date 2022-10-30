9dbaad44fe2367fb71903d8e0a3d79badbcf61ce
sheldygg <chikibamtioma@gmail.com>
2022-10-31 00:19:54 +0300

    update test_user

--- tests/test_api/test_types/test_user.py
+++ tests/test_api/test_types/test_user.py
@@ -14,7 +14,7 @@ class TestUser:
             ["User", " ", "User  "],
             [" ", " ", "   "],
         ],
-    )
-    def test_full_name(self, first: str, last: str, result: bool):
+    )        
+    def test_get_mention_html(self, first: str, last: str, result: bool):
         user = User(id=42, is_bot=False, first_name=first, last_name=last)
-        assert user.full_name == result
+        assert user.mention_html()
