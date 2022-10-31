294210e44aa744927127d69efedb876dfb1fd3d6
sheldygg <chikibamtioma@gmail.com>
2022-10-31 09:56:29 +0200

    update tests

--- tests/test_api/test_types/test_user.py
+++ tests/test_api/test_types/test_user.py
@@ -18,3 +18,33 @@ class TestUser:
     def test_full_name(self, first: str, last: str, result: bool):
         user = User(id=42, is_bot=False, first_name=first, last_name=last)
         assert user.full_name == result
+
+    @pytest.mark.parametrize(
+        "first,last,result",
+        [
+            ["User", None, "User"],
+            ["", None, ""],
+            [" ", None, " "],
+            ["User", "Name", "User Name"],
+            ["User", " ", "User  "],
+            [" ", " ", "   "],
+        ],
+    )
+    def test_get_mention_markdown(self, first: str, last: str, result: bool):
+        user = User(id=42, is_bot=False, first_name=first, last_name=last)
+        assert user.mention_markdown()
+
+    @pytest.mark.parametrize(
+        "first,last,result",
+        [
+            ["User", None, "User"],
+            ["", None, ""],
+            [" ", None, " "],
+            ["User", "Name", "User Name"],
+            ["User", " ", "User  "],
+            [" ", " ", "   "],
+        ],
+    )
+    def test_get_mention_html(self, first: str, last: str, result: bool):
+        user = User(id=42, is_bot=False, first_name=first, last_name=last)
+        assert user.mention_html()
