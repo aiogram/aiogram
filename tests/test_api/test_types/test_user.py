293186cc080062a31e5d6966d29770ffa8fa847b
sheldygg <chikibamtioma@gmail.com>
2022-10-31 09:53:24 +0300

    after black and isort using

--- tests/test_api/test_types/test_user.py
+++ tests/test_api/test_types/test_user.py
@@ -14,7 +14,7 @@ class TestUser:
             ["User", " ", "User  "],
             [" ", " ", "   "],
         ],
-    )        
+    )
     def test_get_mention_html(self, first: str, last: str, result: bool):
         user = User(id=42, is_bot=False, first_name=first, last_name=last)
         assert user.mention_html()
