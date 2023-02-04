83a6b031149c002e6797e10fa2df85940d4492ee
tg:@mars_hall <mars.hall.tg.fck@gmail.com>
2023-02-04 22:54:51 +0500

    adapt ChatPermissions for Bot API 6.5 (#1113)
    * adapt ChatPermissions for Bot API 6.5
    
    * Update aiogram/types/chat_permissions.py
    
    ---------
    
    Co-authored-by: Alex Root Junior <jroot.junior@gmail.com>

--- aiogram/types/chat_permissions.py
+++ aiogram/types/chat_permissions.py
@@ -9,7 +9,13 @@ class ChatPermissions(base.TelegramObject):
     https://core.telegram.org/bots/api#chatpermissions
     """
     can_send_messages: base.Boolean = fields.Field()
-    can_send_media_messages: base.Boolean = fields.Field()
+    can_send_media_messages: base.Boolean = fields.Field()  # Deprecated since Bot API 6.5
+    can_send_audios: base.Boolean = fields.Field()
+    can_send_documents: base.Boolean = fields.Field()
+    can_send_photos: base.Boolean = fields.Field()
+    can_send_videos: base.Boolean = fields.Field()
+    can_send_video_notes: base.Boolean = fields.Field()
+    can_send_voice_notes: base.Boolean = fields.Field()
     can_send_polls: base.Boolean = fields.Field()
     can_send_other_messages: base.Boolean = fields.Field()
     can_add_web_page_previews: base.Boolean = fields.Field()
@@ -21,6 +27,12 @@ class ChatPermissions(base.TelegramObject):
     def __init__(self,
                  can_send_messages: base.Boolean = None,
                  can_send_media_messages: base.Boolean = None,
+                 can_send_audios: base.Boolean = None,
+                 can_send_documents: base.Boolean = None,
+                 can_send_photos: base.Boolean = None,
+                 can_send_videos: base.Boolean = None,
+                 can_send_video_notes: base.Boolean = None,
+                 can_send_voice_notes: base.Boolean = None,
                  can_send_polls: base.Boolean = None,
                  can_send_other_messages: base.Boolean = None,
                  can_add_web_page_previews: base.Boolean = None,
@@ -32,6 +44,12 @@ class ChatPermissions(base.TelegramObject):
         super(ChatPermissions, self).__init__(
             can_send_messages=can_send_messages,
             can_send_media_messages=can_send_media_messages,
+            can_send_audios=can_send_audios,
+            can_send_documents=can_send_documents,
+            can_send_photos=can_send_photos,
+            can_send_videos=can_send_videos,
+            can_send_video_notes=can_send_video_notes,
+            can_send_voice_notes=can_send_voice_notes,
             can_send_polls=can_send_polls,
             can_send_other_messages=can_send_other_messages,
             can_add_web_page_previews=can_add_web_page_previews,
@@ -39,4 +57,5 @@ class ChatPermissions(base.TelegramObject):
             can_invite_users=can_invite_users,
             can_pin_messages=can_pin_messages,
             can_manage_topics=can_manage_topics,
+            **kwargs
         )
