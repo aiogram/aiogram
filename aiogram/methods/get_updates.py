2c756a1689a57d91fa1ebdbfa2f5c7bb3bf18a2c
Alex Root Junior <jroot.junior@gmail.com>
2023-04-08 18:31:38 +0300

    Update API docs

--- aiogram/methods/get_updates.py
+++ aiogram/methods/get_updates.py
@@ -23,7 +23,7 @@ class GetUpdates(TelegramMethod[List[Update]]):
     __api_method__ = "getUpdates"
 
     offset: Optional[int] = None
-    """Identifier of the first update to be returned. Must be greater by one than the highest among the identifiers of previously received updates. By default, updates starting with the earliest unconfirmed update are returned. An update is considered confirmed as soon as :class:`aiogram.methods.get_updates.GetUpdates` is called with an *offset* higher than its *update_id*. The negative offset can be specified to retrieve updates starting from *-offset* update from the end of the updates queue. All previous updates will forgotten."""
+    """Identifier of the first update to be returned. Must be greater by one than the highest among the identifiers of previously received updates. By default, updates starting with the earliest unconfirmed update are returned. An update is considered confirmed as soon as :class:`aiogram.methods.get_updates.GetUpdates` is called with an *offset* higher than its *update_id*. The negative offset can be specified to retrieve updates starting from *-offset* update from the end of the updates queue. All previous updates will be forgotten."""
     limit: Optional[int] = None
     """Limits the number of updates to be retrieved. Values between 1-100 are accepted. Defaults to 100."""
     timeout: Optional[int] = None
--- aiogram/types/input_sticker.py
+++ aiogram/types/input_sticker.py
@@ -17,7 +17,7 @@ class InputSticker(TelegramObject):
     """
 
     sticker: Union[InputFile, str]
-    """The added sticker. Pass a *file_id* as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. Animated and video stickers can't be uploaded via HTTP URL. :ref:`More information on Sending Files » <sending-files>`"""
+    """The added sticker. Pass a *file_id* as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, upload a new one using multipart/form-data, or pass 'attach://<file_attach_name>' to upload a new one using multipart/form-data under <file_attach_name> name. Animated and video stickers can't be uploaded via HTTP URL. :ref:`More information on Sending Files » <sending-files>`"""
     emoji_list: List[str]
     """List of 1-20 emoji associated with the sticker"""
     mask_position: Optional[MaskPosition] = None
