db21da5f5cb64f614b605a9358430610bebd8b91
sheldy <85823514+sheldygg@users.noreply.github.com>
2023-05-15 18:00:22 +0300

    fix typehint

--- aiogram/webhook/aiohttp_server.py
+++ aiogram/webhook/aiohttp_server.py
@@ -132,7 +132,7 @@ class BaseRequestHandler(ABC):
         pass
 
     @abstractmethod
-    def verify_secret(self, secret_token: str) -> bool:
+    def verify_secret(self, telegram_secret_token: str) -> bool:
         pass
 
     async def _background_feed_update(self, bot: Bot, update: Dict[str, Any]) -> None:
@@ -224,7 +224,7 @@ class SimpleRequestHandler(BaseRequestHandler):
         self.bot = bot
         self.secret_token = secret_token
 
-    def verify_secret(self, telegram_secret_token: str) -> bool:
+    def verify_secret(self, telegram_secret_token: Optional[str]) -> bool:
         if self.secret_token:
             return secrets.compare_digest(telegram_secret_token, self.secret_token)
         return True
