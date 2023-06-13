7f04a33a1e6ca5308c0aa8632d73d9c9fc72e19d
dsodx <dsodx@pm.me>
2023-06-13 22:34:04 +0300

    Clear `fsm.storage.__init__.py`

--- aiogram/fsm/__init__.py
+++ aiogram/fsm/__init__.py
@@ -1,40 +1,9 @@
-from contextlib import suppress
-
 from .context import FSMContext
 from .middleware import FSMContextMiddleware
 from .state import State, StatesGroup, StatesGroupMeta
-from .storage import (
-    BaseEventIsolation,
-    BaseStorage,
-    DisabledEventIsolation,
-    MemoryStorage,
-    MemoryStorageRecord,
-    SimpleEventIsolation,
-    StorageKey,
-)
-
-with suppress(ImportError):
-    from .storage import (
-        DefaultKeyBuilder,
-        KeyBuilder,
-        RedisEventIsolation,
-        RedisStorage,
-    )
-
 from .strategy import FSMStrategy, apply_strategy
 
 __all__ = (
-    "StorageKey",
-    "BaseStorage",
-    "BaseEventIsolation",
-    "MemoryStorageRecord",
-    "MemoryStorage",
-    "DisabledEventIsolation",
-    "SimpleEventIsolation",
-    "KeyBuilder",
-    "DefaultKeyBuilder",
-    "RedisStorage",
-    "RedisEventIsolation",
     "FSMContext",
     "State",
     "StatesGroup",
