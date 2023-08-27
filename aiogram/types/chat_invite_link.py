906895a14a0e77b5004ba2f01e8a6e5a53aae20c
Alex Root Junior <jroot.junior@gmail.com>
2023-08-27 17:04:29 +0300

    Cleanup imports

--- aiogram/types/chat_invite_link.py
+++ aiogram/types/chat_invite_link.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import datetime
 from typing import TYPE_CHECKING, Any, Optional
 
 from .base import TelegramObject
--- aiogram/types/chat_member_banned.py
+++ aiogram/types/chat_member_banned.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import datetime
 from typing import TYPE_CHECKING, Any, Literal
 
 from ..enums import ChatMemberStatus
