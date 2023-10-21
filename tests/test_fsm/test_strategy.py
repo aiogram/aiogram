c18975e7955ede21133ebef64f13eed37da6b56f
ZeroN <128647503+OnaZeroN@users.noreply.github.com>
2023-10-22 01:36:18 +0500

    Update test_strategy.py

--- tests/test_fsm/test_strategy.py
+++ tests/test_fsm/test_strategy.py
@@ -27,9 +27,9 @@ class TestStrategy:
             [FSMStrategy.USER_IN_TOPIC, CHAT, CHAT],
             [FSMStrategy.USER_IN_TOPIC, PRIVATE, PRIVATE],
             [FSMStrategy.USER_IN_TOPIC, THREAD, THREAD],
-            [FSMStrategy.CHAT_TOPIC, CHAT, (CHAT_ID, None, None)],
-            [FSMStrategy.CHAT_TOPIC, PRIVATE, (USER_ID, None, None)],
-            [FSMStrategy.CHAT_TOPIC, THREAD, (CHAT_ID, None, THREAD_ID)],
+            [FSMStrategy.CHAT_TOPIC, CHAT, (CHAT_ID, CHAT_ID, None)],
+            [FSMStrategy.CHAT_TOPIC, PRIVATE, PRIVATE],
+            [FSMStrategy.CHAT_TOPIC, THREAD, (CHAT_ID, CHAT_ID, THREAD_ID)],
         ],
     )
     def test_strategy(self, strategy, case, expected):
