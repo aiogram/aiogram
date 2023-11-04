32d5f79a25cbc36923571a05026eba514eb0d835
RootShinobi <111008396+RootShinobi@users.noreply.github.com>
2023-11-04 20:03:44 +0200

    optimized CallableMixin

--- aiogram/dispatcher/event/handler.py
+++ aiogram/dispatcher/event/handler.py
@@ -21,19 +21,22 @@ CallbackType = Callable[..., Any]
 class CallableMixin:
     callback: CallbackType
     awaitable: bool = field(init=False)
-    spec: inspect.FullArgSpec = field(init=False)
+    params: set[str] = field(init=False)
+    varkw: bool = field(init=False)
 
     def __post_init__(self) -> None:
         callback = inspect.unwrap(self.callback)
         self.awaitable = inspect.isawaitable(callback) or inspect.iscoroutinefunction(callback)
-        self.spec = inspect.getfullargspec(callback)
+        spec = inspect.getfullargspec(callback)
+        self.params = {*spec.args, *spec.kwonlyargs}
+        self.varkw = spec.varkw is not None
 
     def _prepare_kwargs(self, kwargs: Dict[str, Any]) -> Dict[str, Any]:
-        if self.spec.varkw:
+        if self.varkw:
             return kwargs
 
         return {
-            k: v for k, v in kwargs.items() if k in self.spec.args or k in self.spec.kwonlyargs
+            k: kwargs[k] for k in self.params if k in kwargs
         }
 
     async def call(self, *args: Any, **kwargs: Any) -> Any:
