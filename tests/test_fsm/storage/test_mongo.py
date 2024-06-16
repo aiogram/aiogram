1df3adaba1889ac95f12b8cce702ff37fc387e14
Rishat-F <66554797+Rishat-F@users.noreply.github.com>
2024-06-17 00:55:59 +0300

    Fail redis and mongo tests if incorrect URI provided + some storages tests refactoring (#1510)
    * Smaller timeout for MongoStorage connection
    
    By default serverSelectionTimeoutMS=30000. This is too much
    
    * Correct ConnectionError for RedisStorage in tests
    
    * Remove unused import in conftest.py
    
    * Refactor skipping redis and mongo tests
    
    * Fail redis and mongo tests if incorrect URI
    
    If incorrect URIs provided to "--redis" and/or "--mongo" options
    tests should fail with ERRORs instead of skipping.
    Otherwise the next scenario is possible:
      1) developer breaks RedisStorage and/or MongoStorage code
      2) tests are run with incorrect redis and/or mongo URIs
         provided by "--redis" and "--mongo" options.
         For example, wrong port specified.
      3) tests pass because skipping doesn't fail tests run
      4) developer or reviewer doesn't notice
         that redis and/or mongo tests were skipped
      5) broken code gets in codebase
    
    * Remove unused fixtures passing in storages tests
    
    * Define create_storage_key fixture in conftest.py
    
    * Linters formatting
    
    * Changes description
    
    * Revert "Smaller timeout for MongoStorage connection"
    
    This reverts commit d88b7ec61264385a8f8ee42d5b20267156ebb9c5.
    
    * Smaller timeout for MongoStorage connection in tests
    
    The default 30s timeout is too long
    
    * Add test for MongoStorage for 100% coverage
    
    * Linters formatting
    
    * Move skipping redis/mongo tests in earlier fixtures
    
    * Replace vars with constants in conftest.py
    
    * Linters formatting

--- tests/test_fsm/storage/test_mongo.py
+++ tests/test_fsm/storage/test_mongo.py
@@ -1,17 +1,19 @@
 import pytest
+from pymongo.errors import PyMongoError
 
 from aiogram.fsm.state import State
 from aiogram.fsm.storage.mongo import MongoStorage, StorageKey
-from tests.mocked_bot import MockedBot
+from tests.conftest import CHAT_ID, USER_ID
 
 PREFIX = "fsm"
-CHAT_ID = -42
-USER_ID = 42
 
 
-@pytest.fixture(name="storage_key")
-def create_storage_key(bot: MockedBot):
-    return StorageKey(chat_id=CHAT_ID, user_id=USER_ID, bot_id=bot.id)
+async def test_get_storage_passing_only_url(mongo_server):
+    storage = MongoStorage.from_url(url=mongo_server)
+    try:
+        await storage._client.server_info()
+    except PyMongoError as e:
+        pytest.fail(str(e))
 
 
 async def test_update_not_existing_data_with_empty_dictionary(
