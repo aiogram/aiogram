094f219072339d131fc57460951a3bc2d075d2b1
Oleg A <t0rr@mail.ru>
2024-07-05 01:04:15 +0300

    fix tests (#1534)

--- tests/test_api/test_types/test_message.py
+++ tests/test_api/test_types/test_message.py
@@ -68,6 +68,8 @@ from aiogram.types import (
     Location,
     MessageAutoDeleteTimerChanged,
     MessageEntity,
+    PaidMediaInfo,
+    PaidMediaPhoto,
     PassportData,
     PhotoSize,
     Poll,
@@ -329,6 +331,22 @@ TEST_MESSAGE_CHANNEL_CHAT_CREATED = Message(
     date=datetime.datetime.now(),
     channel_chat_created=True,
     chat=Chat(id=-10042, type="channel"),
+    from_user=User(id=42, is_bot=False, first_name="Test"),
+)
+TEST_MESSAGE_PAID_MEDIA = Message(
+    message_id=42,
+    date=datetime.datetime.now(),
+    paid_media=PaidMediaInfo(
+        star_count=100500,
+        paid_media=[
+            PaidMediaPhoto(
+                photo=[
+                    PhotoSize(file_id="file id", file_unique_id="file id", width=42, height=42)
+                ],
+            )
+        ],
+    ),
+    chat=Chat(id=42, type="private"),
     from_user=User(id=42, is_bot=False, first_name="Test"),
 )
 TEST_MESSAGE_PASSPORT_DATA = Message(
@@ -603,6 +621,7 @@ MESSAGES_AND_CONTENT_TYPES = [
     [TEST_MESSAGE_GROUP_CHAT_CREATED, ContentType.GROUP_CHAT_CREATED],
     [TEST_MESSAGE_SUPERGROUP_CHAT_CREATED, ContentType.SUPERGROUP_CHAT_CREATED],
     [TEST_MESSAGE_CHANNEL_CHAT_CREATED, ContentType.CHANNEL_CHAT_CREATED],
+    [TEST_MESSAGE_PAID_MEDIA, ContentType.PAID_MEDIA],
     [TEST_MESSAGE_PASSPORT_DATA, ContentType.PASSPORT_DATA],
     [TEST_MESSAGE_PROXIMITY_ALERT_TRIGGERED, ContentType.PROXIMITY_ALERT_TRIGGERED],
     [TEST_MESSAGE_POLL, ContentType.POLL],
@@ -669,6 +688,7 @@ MESSAGES_AND_COPY_METHODS = [
     [TEST_MESSAGE_GROUP_CHAT_CREATED, None],
     [TEST_MESSAGE_SUPERGROUP_CHAT_CREATED, None],
     [TEST_MESSAGE_CHANNEL_CHAT_CREATED, None],
+    [TEST_MESSAGE_PAID_MEDIA, None],
     [TEST_MESSAGE_PASSPORT_DATA, None],
     [TEST_MESSAGE_PROXIMITY_ALERT_TRIGGERED, None],
     [TEST_MESSAGE_POLL, SendPoll],
