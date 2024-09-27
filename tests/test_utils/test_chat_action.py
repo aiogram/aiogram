5cd958c5477330270c5c293682d53b11c24309dc
Carl Smedstad <carl.smedstad@protonmail.com>
2024-09-27 20:51:37 +0200

    Fix pytest-asyncio deprecation warning in test
    Introduced in pytest-asyncio in:
    https://github.com/pytest-dev/pytest-asyncio/pull/648

--- tests/test_utils/test_chat_action.py
+++ tests/test_utils/test_chat_action.py
@@ -13,9 +13,10 @@ from tests.mocked_bot import MockedBot
 
 
 class TestChatActionSender:
-    async def test_wait(self, bot: Bot, event_loop: asyncio.BaseEventLoop):
+    async def test_wait(self, bot: Bot):
         sender = ChatActionSender.typing(bot=bot, chat_id=42)
-        event_loop.call_soon(sender._close_event.set)
+        loop = asyncio.get_running_loop()
+        loop.call_soon(sender._close_event.set)
         start = time.monotonic()
         await sender._wait(1)
         assert time.monotonic() - start < 1
