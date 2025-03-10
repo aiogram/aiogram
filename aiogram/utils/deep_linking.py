925616ff79e9f8519b526bcaedba9341906c109d
naz <85476822+welaskez@users.noreply.github.com>
2025-03-10 05:29:28 +0500

    Added support of "startapp" deep links with "appname" param (#1651)
    * Added "startapp" deep link support
    
    * Remove link_type param, added create_startapp_link method
    
    * Write tests for create_startapp_link method
    
    * Refactor with black & isort
    
    * Added CHANGELOG
    
    * Added support of direct mini app links
    
    * Added CHANGELOG

--- aiogram/utils/deep_linking.py
+++ aiogram/utils/deep_linking.py
@@ -82,6 +82,7 @@ async def create_startapp_link(
     bot: Bot,
     payload: str,
     encode: bool = False,
+    app_name: Optional[str] = None,
     encoder: Optional[Callable[[bytes], bytes]] = None,
 ) -> str:
     """
@@ -93,6 +94,7 @@ async def create_startapp_link(
     :param bot: bot instance
     :param payload: args passed with /start
     :param encode: encode payload with base64url or custom encoder
+    :param app_name: if you want direct mini app link
     :param encoder: custom encoder callable
     :return: link
     """
@@ -101,6 +103,7 @@ async def create_startapp_link(
         username=cast(str, username),
         link_type="startapp",
         payload=payload,
+        app_name=app_name,
         encode=encode,
         encoder=encoder,
     )
@@ -110,6 +113,7 @@ def create_deep_link(
     username: str,
     link_type: Literal["start", "startgroup", "startapp"],
     payload: str,
+    app_name: Optional[str] = None,
     encode: bool = False,
     encoder: Optional[Callable[[bytes], bytes]] = None,
 ) -> str:
@@ -117,8 +121,9 @@ def create_deep_link(
     Create deep link.
 
     :param username:
-    :param link_type: `start` or `startgroup`
+    :param link_type: `start`, `startgroup` or `startapp`
     :param payload: any string-convertible data
+    :param app_name: if you want direct mini app link
     :param encode: encode payload with base64url or custom encoder
     :param encoder: custom encoder callable
     :return: deeplink
@@ -138,4 +143,9 @@ def create_deep_link(
     if len(payload) > 64:
         raise ValueError("Payload must be up to 64 characters long.")
 
-    return create_telegram_link(username, **{cast(str, link_type): payload})
+    if not app_name:
+        deep_link = create_telegram_link(username, **{cast(str, link_type): payload})
+    else:
+        deep_link = create_telegram_link(username, app_name, **{cast(str, link_type): payload})
+
+    return deep_link
